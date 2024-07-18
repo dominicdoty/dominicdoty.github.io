5fe91277de3e99e7edba8adf4bbf430d50b3c0b7
Dominic Doty <doty.dominic@gmail.com>
2024-07-17 21:46:11 -0600

    Add 404 page to post route to handle failed param validation

---
+++ src/routes/[year=year]/[month=month]/[day=day]/[post]/+error.svelte
@@ -0,0 +1,9 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<div class="section">
+	<div class="container is-max-desktop has-text-centered">
+		<h1 class="title">{$page.status}: {$page.error.message}</h1>
+	</div>
+</div>
