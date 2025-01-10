855a998f066f05923434c8113c28fe2309db18e3
Dominic Doty <doty.dominic@gmail.com>
2025-01-10 16:57:06 -0700

    Try out adapter-cloudflare

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import { mdsvex } from 'mdsvex';
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import remarkExcerpt from 'mdsvex-excerpt';
 // svelte.config.js
@@ -29,9 +29,7 @@ const config = {
 	],
 
 	kit: {
-		// note that adapter-cloudflare is available for cloudflare pages if static rendering isn't enough
-		adapter: adapter({ fallback: '404.html' }),
-
+		adapter: adapter(),
 		alias: {
 			$src: './src'
 		}
