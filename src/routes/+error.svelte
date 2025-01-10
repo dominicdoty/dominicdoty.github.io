f9d6223d79ff4e30f7e092a3303110da797891f7
Dominic Doty <doty.dominic@gmail.com>
2025-01-10 16:12:18 -0700

    Add an error route to fall back on (matches the 404)

---
+++ src/routes/+error.svelte
@@ -0,0 +1,10 @@
+<script>
+</script>
+
+<div class="section">
+	<div class="container is-max-desktop has-text-centered">
+		<h1 class="title">404</h1>
+		<h2 class="subtitle">Not Found</h2>
+		<p>No idea what you're on about mate.</p>
+	</div>
+</div>
